2e6ccd0f98386f7bb8dc6a296a5bc84f2caabc43
luis1412 <luisherradorcruz@gmail.com>
2023-12-18 10:03:18 +0100

    Agregar archivos de proyecto.

---
+++ Models/Canciones.cs
@@ -0,0 +1,10 @@
+﻿namespace Musica2.Models
+{
+    public class Canciones
+    {
+        public int Id { get; set; }
+        public string nombre { get; set; }
+
+
+    }
+}
---
+++ Models/Usuarios.cs
@@ -0,0 +1,6 @@
+﻿namespace Musica2.Models
+{
+    public class Usuarios
+    {
+    }
+}
